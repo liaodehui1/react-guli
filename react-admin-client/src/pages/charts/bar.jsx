a72d1988c49b8fae5ec824a73079cff2199e15a1
liaodehui <1959956996@qq.com>
2020-03-20 00:20:31 +0800

    use-echarts/bizcharts

--- react-admin-client/src/pages/charts/bar.jsx
+++ react-admin-client/src/pages/charts/bar.jsx
@@ -1,12 +1,59 @@
-import React, { Component } from 'react';
+import React, { Component } from 'react'
+import { Card, Button } from 'antd'
+import ReactEcharts from 'echarts-for-react'
 
 /**
- * 树状图
+ * 柱状图
  */
 export default class Bar extends Component {
+  state = {
+    sales: [5, 20, 36, 10, 10, 20],
+    inventorys: [15, 30, 46, 20, 20, 40]
+  }
+  getOption = () => {
+    const { sales, inventorys } = this.state
+    return {
+      title: {
+        text: 'ECharts 入门示例'
+      },
+      tooltip: {},
+      legend: {
+        data: ['销量', '库存']
+      },
+      xAxis: {
+        data: ["衬衫", "羊毛衫", "雪纺衫", "裤子", "高跟鞋", "袜子"]
+      },
+      yAxis: {},
+      series: [
+        {
+          name: '销量',
+          type: 'bar',
+          data: sales
+        },
+        {
+          name: '库存',
+          type: 'bar',
+          data: inventorys
+        },
+      ]
+    }
+  }
+  update = () => {
+    this.setState(state => ({
+      sales:state.sales.map(item => item + 1),
+      inventorys:state.inventorys.map(item => item - 1)
+    }))
+  }
   render() {
     return (
-      <div> Bar </div>
+      <div>
+        <Card>
+          <Button type='primary' onClick={this.update}>更新</Button>
+        </Card>
+        <Card title='柱状图一'>
+          <ReactEcharts option={this.getOption()} style={{ height: 300 }} />
+        </Card>
+      </div>
     );
   }
 }
